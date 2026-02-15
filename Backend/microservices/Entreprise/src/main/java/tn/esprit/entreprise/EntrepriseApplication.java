d264738897268fb6a3058e7d2e0db8b11270b0b8
Mariem <mariem.abidi@esprit.tn>
2026-02-15 17:28:43 +0100

    microservice change

---
+++ Backend/microservices/Entreprise/src/main/java/tn/esprit/entreprise/EntrepriseApplication.java
@@ -0,0 +1,15 @@
+package tn.esprit.entreprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EntrepriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrepriseApplication.class, args);
+	}
+
+}
---
+++ Backend/microservices/subscription/src/main/java/tn/esprit/subscription/SubscriptionApplication.java
@@ -0,0 +1,15 @@
+package tn.esprit.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionApplication.class, args);
+
+}
+}
